a9e70467a65c58c9d98675244ab5f41a5eb98908
molt0bene <alisar3@yandex.ru>
2023-06-04 12:34:04 +0300

    added web page

--- src/web_app/app.py
+++ src/web_app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 app.run('0.0.0.0', 5050)
